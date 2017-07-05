5966bb071ba842736525c4907e3e3a65070a70a8
wangleitheone <1183510062@qq.com>
2017-07-05 14:38:38 +0800

    初步完成基本功能，剩回退未解决，以及如何保证目标点不被抹去，以及目标点，箱子同时存在问题

--- 推箱子/画图形.cpp
+++ 推箱子/画图形.cpp
@@ -2,25 +2,39 @@
 #include <conio.h>
 void wall(int x,int y)//画一块墙，参数为墙中心点xy坐标
 {
+	x = 20 + 40 * x;
+	y = 20 + 40 * y;
 	setlinecolor(RGB(225, 0, 0));
 	setfillcolor(RGB(225,0,0));
-	fillrectangle(x - 20, y - 20, x + 20, y + 20);
+	solidrectangle(x - 20, y - 20, x + 20, y + 20);
 }
 void box(int x,int y)//画一个箱子，参数为箱子中心点xy坐标
 {
+	x = 20 + 40 * x;
+	y = 20 + 40 * y;
 	setlinecolor(RGB(0, 225, 0));
 	setfillcolor(RGB(0, 225, 0));
-	fillrectangle(x - 20, y - 20, x + 20, y + 20);
+	solidrectangle(x - 20, y - 20, x + 20, y + 20);
 }
-void ball(int x, int y)//画一个目标点，参数为目标点中心点xy坐标
+void point(int x, int y)//画一个目标点，参数为目标点中心点xy坐标
 {
+	x = 20 + 40 * x;
+	y = 20 + 40 * y;
 	setlinecolor(RGB(225, 0, 0));
 	setfillcolor(RGB(225, 0, 0));
-	fillcircle(x,y,10);
+	solidcircle(x,y,10);
 }
 void people(int x, int y)//画一个人，参数为人中心点xy坐标
 {
+	x = 20 + 40 * x;
+	y = 20 + 40 * y;
 	setlinecolor(RGB(0, 0, 225));
 	setfillcolor(RGB(0, 0, 225));
-	fillrectangle(x - 15, y - 15, x + 15, y + 15);
+	solidrectangle(x - 15, y - 15, x + 15, y + 15);
+}
+void clean(int x, int y)//清空一片区域
+{
+	x = 20 + 40 * x;
+	y = 20 + 40 * y;
+	clearrectangle(x - 20, y - 20, x + 20, y + 20);
 }
--- 推箱子/画图形.h
+++ 推箱子/画图形.h
@@ -1,4 +1,5 @@
 void wall(int x, int y);
 void box(int x, int y);
-void ball(int x, int y);
+void point(int x, int y);
 void people(int x, int y);
+void clean(int x, int y);
